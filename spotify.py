bf0679402fab62875119a2be963cbb74489298dd
Aryan <adusharma22@gmail.com>
2021-06-26 00:57:15 +0530

    bug fixes

--- spotify.py
+++ spotify.py
@@ -3,12 +3,13 @@ from spotipy.client import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 
 # ---------------------TO DO ---------------------------- 
-#   make a get_name command to get name from spotify link
+#   make a get_name command to get name from spotify link for a single song
+#   and make it program faster
 # --------------------------------------------------------
 
 
-client = '3298160dcfbd4f98bab39a3361036577' 
-secret = '38b00c4aeb664bb3af894c7b1dd270bf'
+client = 'client key' 
+secret = 'secret key'
 
 client_credentials_manager = SpotifyClientCredentials(client_id=client, client_secret=secret)
 spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
